0636bc5fb5b53db39da7786c1d9060ee2cbac8af
Jeroen Knoops <jeroen.knoops@philips.com>
2022-04-09 10:16:31 +0100

    Add Philips-labs and limit number of repos
    Signed-off-by: Jeroen Knoops <jeroen.knoops@philips.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,52 +1,21 @@
 import React, { useEffect } from 'react';
 import Layout from '../components/Layout';
 import { graphql } from 'gatsby';
-import { uniq } from 'ramda';
-import { useList } from 'react-use';
 import SEO from '../components/SEO';
-import MainContainer from '../components/MainContainer';
-import PageHeader from '../components/PageHeader';
-import BasicSelect from '../components/BasicSelect';
-import Repositories from '../components/Repositories';
+import Organization from '../components/Organization';
 
 const IndexPage = ({ data, ...props }) => {
-  const languages = uniq(
-    data.github.organization.repositories.nodes.map(x =>
-      x.primaryLanguage ? x.primaryLanguage.name : 'none',
-    ),
-  );
-  const [selectedLanguages, { set }] = useList([]);
-  const [projects, { filter, reset }] = useList(
-    data.github.organization.repositories.nodes,
-  );
-
-  useEffect(() => {
-    console.log('filter');
-    reset();
-    filter(item => {
-      if (selectedLanguages.length === 0) return true;
-      if (!item.primaryLanguage && selectedLanguages.includes('none'))
-        return true;
-      return selectedLanguages.includes(
-        item.primaryLanguage && item.primaryLanguage.name,
-      );
-    });
-  }, [selectedLanguages, filter, reset]);
-
   return (
     <Layout>
       <SEO title="Projects" />
-      <PageHeader title="Projects">
-        <BasicSelect
-          placeholder="Select language"
-          value={selectedLanguages}
-          options={languages}
-          onChange={set}
-        />
-      </PageHeader>
-      <MainContainer>
-        <Repositories data={projects} />
-      </MainContainer>
+      <Organization 
+        organization = "philips-software"
+        projects = {data.github.psOrganization.repositories.nodes}
+      />
+      <Organization 
+        organization = "philips-labs"
+        projects = {data.github.plOrganization.repositories.nodes}
+      />
     </Layout>
   );
 };
@@ -56,8 +25,25 @@ export default IndexPage;
 export const query = graphql`
   {
     github {
-      organization(login: "philips-software") {
-        repositories(first: 100, privacy: PUBLIC, isFork: false) {
+      psOrganization: organization(login: "philips-software") {
+        repositories(
+          first: 12
+          privacy: PUBLIC
+          isFork: false
+          orderBy: {field: STARGAZERS, direction: DESC}
+        ) {
+          nodes {
+            ...Repository
+          }
+        }
+      }
+      plOrganization: organization(login: "philips-labs") {
+        repositories(
+          first: 12
+          privacy: PUBLIC
+          isFork: false
+          orderBy: {field: STARGAZERS, direction: DESC}
+        ) {
           nodes {
             ...Repository
           }
